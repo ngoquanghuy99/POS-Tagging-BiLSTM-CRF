b94e7071c09b23498a009e8e8b267397d098c92e
Huy Ngo <ngoquanghuy1999lp@gmail.com>
2020-11-19 20:30:17 +0700

    train model

---
+++ train.py
@@ -0,0 +1,20 @@
+import numpy as np
+from preprocess import load_data
+from config import *
+from model import create_model
+import tensorflow as tf
+import keras
+from keras.models import Sequential, Model
+from keras.layers import Embedding, Bidirectional, LSTM, Dense, TimeDistributed
+from keras.optimizers import Adam
+import crf
+from crf import CRF
+
+x_train, y_train, x_val, y_val, x_test, y_test, word_index, tag_index, maxlen = load_data()
+max_length = maxlen
+vocab_size = len(word_index) + 1
+
+model = create_model(vocab_size, max_length, embedding_dim, word_index):
+model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_data(x_val, y_val), verbose=1)
+model.save_weights('models/1stmodel.h5')
+    
