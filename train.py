88c4fd34849d3ee3ffa4bb119af87d0fc830c615
Huy Ngo <ngoquanghuy1999lp@gmail.com>
2020-11-20 03:38:34 +0700

    update files

--- train.py
+++ train.py
@@ -17,4 +17,6 @@ vocab_size = len(word_index) + 1
 model = create_model(vocab_size, max_length, embedding_dim, word_index):
 model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_data(x_val, y_val), verbose=1)
 model.save_weights('models/1stmodel.h5')
-    
+
+viterbi_acc = model.evaluate(x_test, y_test)
+print(f"{viterbi_acc[1] * 100}")
